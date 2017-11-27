e90db2720bc28329678a2fa32b3c828217315d61
corydodson <34013175+corydodson@users.noreply.github.com>
2017-11-26 19:25:16 -0500

    Update testingNozzle.m
    Updated file with descriptions

--- testingNozzle.m
+++ testingNozzle.m
@@ -1,25 +1,23 @@
 % boundary conditions
-t0 = 3000;
-p0 = 10*101325;
-species = {'air'};
-moleFrac = 1;
+t0 = 3000; % total temperature [K]
+p0 = 10*101325; % total pressure [Pa]
+species = {'air'}; % species
+moleFrac = 1; % mole fractions of species
 
-rTd = 0.625;
-yt = 1;
-d = 1;
+rTd = 0.625; % radius of curvature downstream of throat
+yt = 1; % half height at minimum geometric area of nozzle
+d = 1; % 1 for axisymmetric, 0 for recatngular cross-section
 
-N = 15;
-mDesign = 5;
-plotting = 1;
+N = 15; % number of seed points along initial value line and wall
+mDesign = 5; % design Mach number
+plotting = 1; % 1 for plotting on to see results in graphical form
 
-tic
 [x,y,u,v,exitPropTable,turnPoly,strtPoly] = nozzle(t0,p0,species,moleFrac,rTd,yt,d,N,mDesign,plotting);
-toc
 
 %%
 disp(exitPropTable)
 
-%%
+%% plot boundary geometry: initial value line and inviscid boundary
 figure
 plot(linspace(x(1,1),x(1,N),N),polyval(turnPoly,linspace(x(1,1),x(1,N),N)))
 hold on
